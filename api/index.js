4cc2a7a55fcca217d1640a37795ac69abbbba112
Avinash Dhar <avinashdhar@Avinashs-MacBook-Air.local>
2023-12-16 08:54:41 +0530

    resolving CORS error

--- api/index.js
+++ api/index.js
@@ -7,6 +7,11 @@ require('dotenv/config');
 const authJwt = require('../helpers/jwt');
 const errorHandler = require('../helpers/error-handler');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(cors());
 app.options('*', cors())
